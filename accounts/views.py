d0786f876cc692f9ad06868959808be3266e8883
Tamim Al Hasan <talhasan06@gmail.com>
2023-12-30 01:20:49 +0600

    commit 13

--- accounts/views.py
+++ accounts/views.py
@@ -27,7 +27,7 @@ class UserLogoutView(LogoutView):
     def get_success_url(self):
         if self.request.user.is_authenticated:
             logout(self.request)
-        return reverse_lazy('home')
+            return reverse_lazy('home')
 
 class UserBankAccountUpdateView(View):
     template_name = 'accounts/profile.html'
