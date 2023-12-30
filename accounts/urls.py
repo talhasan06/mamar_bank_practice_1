acb1168db7400adea6d3505265d9b868c90de521
Tamim Al Hasan <talhasan06@gmail.com>
2023-12-30 14:00:47 +0600

    balance transfer 2

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('login/',UserLoginView.as_view(),name='login'),
     path('logout/',UserLogoutView.as_view(),name='logout'),
     path('profile/', UserBankAccountUpdateView.as_view(), name='profile' ),
-    path('transfer-balance/', TransferBalanceView.as_view(), name='profile' ),
+    path('transfer_balance/', TransferBalanceView.as_view(), name='transfer_balance' ),
 ]
