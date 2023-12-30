1e23237fdd001bb3490adf3f6b44a98928f87226
Tamim Al Hasan <talhasan06@gmail.com>
2023-12-30 19:44:54 +0600

    balance withdraw 4

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import UserBankAccount,UserAddress
+from .models import UserBankAccount,UserAddress,Bank
 
 # Register your models here.
 
 admin.site.register(UserBankAccount)
 admin.site.register(UserAddress)
+admin.site.register(Bank)
